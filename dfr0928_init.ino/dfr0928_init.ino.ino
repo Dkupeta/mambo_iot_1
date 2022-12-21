acdc911f4c267e9a3c067c6a89dfa74e8fed4605
kups <dkupeta@gmail.com>
2022-12-21 22:37:48 +0000

    drf0922 sourcecode

---
+++ dfr0928_init.ino/dfr0928_init.ino.ino
@@ -0,0 +1,17 @@
+#include <DFRobot_Type.h>
+#include <DFRobot_UI.h>
+#include <DFRobot_GDL.h>
+#include <DFRobot_Gesture.h>
+#include <DFRobot_Picdecoder_SD.h>
+#include <graphic.h>
+#include <DFRobot_Touch.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
