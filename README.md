b313544c385ea428c1aca8c8d579475bec477075
kups <dkupeta@gmail.com>
2022-12-22 21:23:12 +0000

    Wiring
    Wire display unit
    wire esp

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mambo_iot_1
+This ST7735S 1.8" TFT Display features a resolution of 128×160 and SPI (4-wire) communication to be used as main display
+Development to be either:
+	esp32
+	esp8266
+	sam zero
+	
+	
+for reference refer to;
+https://wiki.dfrobot.com/SKU_DFR0928_Fermion_1.8_Inches_128_160_IPS_TFT_LCD_Display_with_MicroSD_Card_Slot_Breakout#ino1
--- dfr0928_init.ino/dfr0928_init.ino.ino
+++ dfr0928_init.ino/dfr0928_init.ino.ino
@@ -1,14 +1,38 @@
-#include <DFRobot_Type.h>
-#include <DFRobot_UI.h>
+//#include <DFRobot_Type.h>
+//#include <DFRobot_UI.h>
 #include <DFRobot_GDL.h>
-#include <DFRobot_Gesture.h>
-#include <DFRobot_Picdecoder_SD.h>
-#include <graphic.h>
-#include <DFRobot_Touch.h>
+//#include <DFRobot_Gesture.h>
+//#include <DFRobot_Picdecoder_SD.h>
+//#include <graphic.h>
+//#include <DFRobot_Touch.h>
+
+/*M0*/
+#if defined ARDUINO_SAM_ZERO
+#define TFT_DC  7
+#define TFT_CS  5
+#define TFT_RST 6
+/*ESP32 ESP8266*/
+#elif defined(ESP32)
+#define TFT_DC  D2
+#define TFT_CS  D6
+#define TFT_RST D3
+/*ESP8266*/
+#elif defined(ESP8266)
+#define TFT_DC  D4
+#define TFT_CS  D6
+#define TFT_RST D5
+/* AVR series mainboard */
+#else
+#define TFT_DC  2
+#define TFT_CS  3
+#define TFT_RST 4
+#endif
+
+DFRobot_ST7735_128x160_HW_SPI screen(/*dc=*/TFT_DC, /*cs=*/TFT_CS, /*rst=*/TFT_RST);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  screen.begin();
 }
 
 void loop() {
